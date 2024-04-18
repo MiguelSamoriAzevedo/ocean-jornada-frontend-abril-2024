5775e3c9296579ae5946fbd33931a3cd250aca87
Christiano Miguel Samori de Azevedo <160412925+cmiguelsam@users.noreply.github.com>
2024-04-18 23:51:40 +0000

    Criação do Card com estilização do CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   
   return (
     <>
-      Hello World!
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src= "https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
     </>
   )
 }
